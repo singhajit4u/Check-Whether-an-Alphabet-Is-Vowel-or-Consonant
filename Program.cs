75dd826a3ccb802d49eae888f53f2b1d1eb3f02e
NFO-2 <NFO-2@NFO-2>
2023-03-17 11:33:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Check_Whether_an_Alphabet_Is_Vowel_or_Consonant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the alphabet to check Vowel or Consonent:");
+            char ch=Convert.ToChar(Console.ReadLine());
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.Write($"{ch} :Vowel");    
+            }
+            else
+            {
+                Console.Write($"{ch} :Consonent");
+
+            }
+        }
+    }
+}
